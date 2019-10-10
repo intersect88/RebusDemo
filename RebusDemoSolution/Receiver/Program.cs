d50d0da6fa284222d15095751f8ea77237f317fa
intersect88 <intersect@anche.no>
2019-10-10 23:34:03 +0200

    remove unused references

--- RebusDemoSolution/Receiver/Program.cs
+++ RebusDemoSolution/Receiver/Program.cs
@@ -2,10 +2,6 @@ using Messages;
 using Rebus.Activation;
 using Rebus.Config;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Receiver
 {
--- RebusDemoSolution/Sender/Program.cs
+++ RebusDemoSolution/Sender/Program.cs
@@ -1,12 +1,7 @@
 using Messages;
 using Rebus.Activation;
 using Rebus.Config;
-using Rebus.Routing.TypeBased;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Sender
 {
@@ -19,10 +14,9 @@ namespace Sender
                 var message = new DemoMessage("Demo Message");
                 var bus = Configure.With(activator)
                     .Transport(t => t.UseRabbitMqAsOneWayClient("amqp://localhost"))
-                    .Routing(r => r.TypeBased().Map<DemoMessage>("Receiver"))
                     .Start();
 
-                    bus.Publish(message).Wait();
+                bus.Publish(message).Wait();
                 Console.ReadLine();
 
             }
